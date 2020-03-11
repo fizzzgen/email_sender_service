5dc6c40ac6aec25769d2e4b97e6d645cd0638737
fizzzgen <fizzzgen@gmail.com>
2020-03-11 22:32:44 +0300

    m

--- requirments.sh
+++ requirments.sh
@@ -1,7 +1,6 @@
-sudo apt-get install python python3 python-pip python3-pip
-pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-pip install pytelegrambotapi
-pip3 install pytelegrambotapi
-pip install Flask
-pip3 install Flask
+apt install python
+apt install python3
+apt install python-pip
+apt install python3-pip
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib pytelegrambotapi Flask mock
+pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib pytelegrambotapi Flask mock
